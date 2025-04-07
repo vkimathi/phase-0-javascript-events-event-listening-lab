8e521063b978e156b9c869dbdc6a5cf478bf8e0e
Victor Kimathi <vkimathi97@gmail.com>
2025-04-07 23:44:24 +0300

    events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+
+const input = document.getElementById('button');
+
+const clickAlert = () => alert('I was clicked!');
+
+  input.addEventListener('click', clickAlert);
 }
+
+addingEventListener()
